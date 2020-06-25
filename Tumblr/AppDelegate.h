c9e960b145d215f888e2078c1e2a87501d3f796a
Angela Xu <xuangela@cs.washington.edu>
2020-06-25 08:35:01 -0700

    Initial Commit

---
+++ Tumblr/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tumblr
+//
+//  Created by Angela Xu on 6/25/20.
+//  Copyright © 2020 Angela Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Tumblr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tumblr
+//
+//  Created by Angela Xu on 6/25/20.
+//  Copyright © 2020 Angela Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
